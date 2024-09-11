535ae3de0ed0678bffa0845753fe06ea5dbf620c
eunsu <songess@naver.com>
2024-09-11 16:45:21 +0900

    [Fix] : 콘솔문 제거

--- src/app/(NavBarCommonLayout)/layout.tsx
+++ src/app/(NavBarCommonLayout)/layout.tsx
@@ -1,13 +1,10 @@
 import NavBar from '@components/all/NavBar/NavBar';
-import { cookies } from 'next/headers';
 
 export default function NavBarCommonLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const cookie = cookies();
-  console.log(cookie.get('refreshToken'));
 
   return (
     <div className="w-full h-full">
