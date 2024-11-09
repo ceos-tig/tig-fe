135634acd57b49bccb4d1d889dcd32e13e794411
eunsu <songess@naver.com>
2024-11-09 16:07:03 +0900

    [Feat] : 검색페이지에서 시간에 대한 필터링 기능 제거

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
       )}
       <LocationCard />
       <DateCard />
-      <TimePickerCard />
+      {/* <TimePickerCard /> */}
       <ButtonCard />
       {isModalOpen && <SearchModal />}
     </main>
